1994c195eabb25252645299088434dcd006b8185
kylerassweiler <109486853+kylerassweiler@users.noreply.github.com>
2022-07-19 21:16:12 +0000

    Initial commit

---
+++ src/components/BlogList.jsx
@@ -0,0 +1,42 @@
+import BlogPost from "./BlogPost";
+import Pagination from "./Pagination";
+import React from "react";
+import blogs from "../data/blogs.json";
+
+const PAGE_SIZES = [15, 25, 50, 100];
+
+function BlogList() {
+  const currentPaginationData = blogs.posts.slice(0, 15);
+
+  const updateRowsPerPage = () => {};
+  const updatePage = () => {};
+
+  return (
+    <div>
+      <Pagination
+        currentPage={1}
+        totalCount={blogs.posts.length}
+        pageSize={15}
+        pageSizeOptions={PAGE_SIZES}
+        onPageChange={updatePage}
+        onPageSizeOptionChange={updateRowsPerPage}
+      />
+      <ul
+        // Do not remove the aria-label below, it is used for Hatchways automation.
+        aria-label="blog list"
+      >
+        {currentPaginationData.map((blog) => (
+          <BlogPost
+            key={blog.id}
+            author={blog.author}
+            title={blog.title}
+            excerpt={blog.excerpt}
+            featureImage={blog.image}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default BlogList;
---
+++ src/hooks/usePagination.js
@@ -0,0 +1,14 @@
+export const DOTS = "...";
+
+function usePagination() {
+  /*
+    Rewrite the logic here to map out the pagination to be displayed
+
+    !!!!!! ATTENTION !!!!!!
+    Please replace this comment here with a description of this hook.
+    
+  */
+  return [1, 2, 3, DOTS, 5];
+}
+
+export default usePagination;
