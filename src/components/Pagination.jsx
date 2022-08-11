b8b49c31dc371e133a9d1a07fb32c3a2c88236a9
Kyle Rassweiler <dev@kylerassweiler.ca>
2022-08-10 20:26:13 -0400

    [#1] Clean up unnecessary logic for button disable

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -42,7 +42,7 @@ function Pagination({
           // Do not remove the aria-label below, it is used for Hatchways automation.
           aria-label="Goto previous page"
           onClick={onPrevious}
-          disabled={currentPage > 1 ? false : true} // change this line to disable a button.
+          disabled={currentPage <= 1} // change this line to disable a button.
         >
           <ChevronLeftIcon />
         </button>
@@ -84,9 +84,7 @@ function Pagination({
           // Do not remove the aria-label below, it is used for Hatchways automation.
           aria-label="Goto next page"
           onClick={onNext}
-          disabled={
-            currentPage < Math.ceil(totalCount / pageSize) ? false : true
-          } // change this line to disable a button.
+          disabled={currentPage >= Math.ceil(totalCount / pageSize)} // change this line to disable a button.
         >
           <ChevronRightIcon />
         </button>
